ba933e5967dbd6e346a79d09e0f74975b0860538
Yunus Berkay İnci <berkayinci25@gmail.com>
2024-04-03 14:04:59 +0300

    Petlebi.com-DataScraper
    PetlebiDataScraper is a Python-based tool designed for extracting detailed product data from www.petlebi.com. It automates web scraping to gather product details, stores the information in JSON, and integrates with MySQL for database management. Ideal for market analysis and data-driven insights.

---
+++ petlebi/petlebi/spiders/Untitled-1.py
@@ -0,0 +1,11 @@
+
+def factorial(number):
+    number=number
+    total=0
+    for i in range(number,1,i-1):
+      fact=number*i
+      total=total+fact
+      
+    print(fact)    
+
+factorial(5)
